75971b173dce1c5cf59d013945fb1b7c7db3e38b
Smokerwolf <97537213+Smokerwolf@users.noreply.github.com>
2022-01-18 18:36:34 +0100

    Update mapper.py

--- exercicio_1.2/mapper.py
+++ exercicio_1.2/mapper.py
@@ -1,13 +1,29 @@
 #!/usr/bin/python3
 
-#Redefine o mapper e o reducer de xeito que devolvan un ficheiro co total de vendas por categoría.
-
+#Importaciones--------
 import sys
+#---------------------
+
+#Variables------------
+#Podría usar un diccionario, cuya clave sea la categoria
+#el método de pago y el valor sea pues el total de ventas
+diccionario = {}
+#---------------------
 
 for line in sys.stdin:
-    
     data = line.strip().split("\t")
     
-    if len(data) == 6:
-        date, time, store, item, cost, payment = data
-        print(f'{item}\t{cost}')
+    #Si encuentra una linea que no tenga 6 elementos, la ignora
+    if len(data) != 6:
+    	continue
+    
+
+    date, time, store, item, cost, payment = data
+    if payment in diccionario:
+        diccionario[payment] += float(cost)
+    else:
+        diccionario.update({ payment : float(cost) })
+    
+for key,value in diccionario.items():
+    print("Método de pago: {} | Cantidad: {}".format(key,format(value, ".2f")))
+#print(diccionario)
